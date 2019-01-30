6003317d7e60a79b11f674064c8fab56aadd25f3
Yousef Alsaeid <macshiba@mac.com>
2019-01-30 22:29:19 +0300

    user can create an address

--- Components/Profile/index.js
+++ Components/Profile/index.js
@@ -1,22 +1,103 @@
 import React, { Component } from "react";
 
 // NativeBase Components
-import { Card, CardItem, Text, Button } from "native-base";
+import {
+  Container,
+  Content,
+  Form,
+  Item,
+  Input,
+  Button,
+  Text
+} from "native-base";
 import authStore from "../../stores/authStore";
+import addressStore from "../../stores/addressStore";
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phone: "",
+      area: "",
+      block: "",
+      street: "",
+      house: "",
+      jaada: "",
+      user: authStore.user
+    };
+  }
   render() {
+    console.log(authStore.user);
     return (
-      <Card>
-        <CardItem>
-          <Button
-            danger
-            onPress={() => alert("You need to implement Logout n00b...")}
-          >
-            <Text>Logout</Text>
-          </Button>
-        </CardItem>
-      </Card>
+      <Container>
+        <Content>
+          <Form>
+            <Item>
+              <Input
+                placeholder="area"
+                autoCapitalize="none"
+                onChangeText={area => this.setState({ area })}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="block"
+                autoCapitalize="none"
+                onChangeText={block => this.setState({ block })}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="street"
+                autoCapitalize="none"
+                onChangeText={street => this.setState({ street })}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="jaada"
+                autoCapitalize="none"
+                onChangeText={jaada => this.setState({ jaada })}
+              />
+            </Item>
+            <Item>
+              <Input
+                placeholder="house"
+                autoCapitalize="none"
+                onChangeText={house => this.setState({ house })}
+              />
+            </Item>
+            <Item last>
+              <Input
+                placeholder="phone"
+                autoCapitalize="none"
+                onChangeText={phone => this.setState({ phone })}
+              />
+            </Item>
+            <Button
+              full
+              onPress={
+                // () => console.log(this.state)
+                () => addressStore.createAddress(this.state)
+                // authStore.loginUser(this.state, this.props.navigation)
+              }
+            >
+              <Text>Create Address</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+
+      // <Card>
+      //   <CardItem>
+      //     <Button
+      //       danger
+      //       onPress={() => authStore.logoutUser(this.props.navigation)}
+      //     >
+      //       <Text>Logout</Text>
+      //     </Button>
+      //   </CardItem>
+      // </Card>
     );
   }
 }
