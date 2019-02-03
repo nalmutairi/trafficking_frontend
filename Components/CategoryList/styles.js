ac2cfb67ea4b2126f275a7233273acf08028b271
A-Alhashem <q8_ash@live.com>
2019-02-03 21:22:46 +0300

    partial edits on android side

--- Components/CategoryList/styles.js
+++ Components/CategoryList/styles.js
@@ -6,10 +6,20 @@ const styles = StyleSheet.create({
     flexWrap: "wrap"
   },
   item: {
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 2, width: 2 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    backgroundColor: "#fff",
     width: Dimensions.get("window").width * 0.45,
     height: 150,
-    backgroundColor: "#E4EBF3",
-    borderWidth: 2,
+    // backgroundColor: "#E4EBF3",
+    elevation: 2, // Android
+    // width: 100,
+    // justifyContent: "center",
+    // alignItems: "center",
+    flexDirection: "row",
+    borderWidth: 1,
     borderColor: "#A7A8B1",
     alignItems: "center",
     justifyContent: "center",
@@ -26,6 +36,19 @@ const styles = StyleSheet.create({
   itemTitle: {
     marginTop: 10
     //test
+  },
+  button: {
+    shadowColor: "rgba(0,0,0, .4)", // IOS
+    shadowOffset: { height: 1, width: 1 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+    backgroundColor: "#fff",
+    elevation: 2, // Android
+    height: 50,
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
   }
 });
 
